a625041d664dc7045680801131bfb562c0544f62
Jatin Chowdhury <jchowdhu@stanford.edu>
2021-11-02 14:05:39 -0700

    Update diodeR and clipper_pot training scripts

--- wdf_py/clipper_pot.py
+++ wdf_py/clipper_pot.py
@@ -2,10 +2,13 @@
 import numpy as np
 from scipy.special import wrightomega
 import matplotlib.pyplot as plt
+
 import tf_wdf as wdf
 from tf_wdf import tf
+
 from tqdm import tqdm
 import pathlib
+import json
 
 from model_utils import *
 
@@ -36,7 +39,7 @@ print(data_in_batched.shape)
 # print(data_target.shape)
 
 # plt.plot(data_in[:, 0])
-plt.plot(data_in_batched[0, :, 1] / 200000)
+plt.plot(np.log(data_in_batched[0, :, 1]) - 8)
 plt.plot(data_in_batched[0, :, 0] / 8)
 plt.plot(data_target[:,0])
 
@@ -52,54 +55,74 @@ class DenseLayer(tf.Module):
     def init_weights(self, batch_size, size1, size2):
         initializer = tf.keras.initializers.Orthogonal()
         init = initializer(shape=(size1, size2))
-        return [init for b in range(batch_size)]
+        return [init for _ in range(batch_size)]
 
     def init_bias(self, batch_size, size):
         initializer = tf.keras.initializers.Zeros()
         init = initializer(shape=(size))
-        return [init for b in range(batch_size)]
+        return [init for _ in range(batch_size)]
+
+    def set_weights(self, json_weights):
+        weights = json_weights[0]
+        self.kernel.assign(np.array([weights]))
+    
+        bias = json_weights[1]
+        self.bias.assign(np.array([bias]))
 
     def __call__(self, input):
         return tf.matmul(input, self.kernel) + self.bias
 
 class RootModel(tf.Module):
-    def __init__(self, in_size, n_layers, hidden_dim, batch_size=1):
+    def __init__(self, json, batch_size=1):
         super(RootModel, self).__init__()
 
         self.a = tf.Variable(initial_value=tf.zeros(1), name='incident_wave')
         self.b = tf.Variable(initial_value=tf.zeros(1), name='reflected_wave')
 
+        in_size = json['in_shape'][-1]
+        layers_json = json['layers']
         self.layers = []
 
-        for n in range(n_layers):
-            in_size = in_size if n == 0 else hidden_dim
-            out_size = 1 if n == n_layers - 1 else hidden_dim
-            self.layers.append(DenseLayer(batch_size, in_size, out_size))
+        prev_size = in_size
+        for l in layers_json:
+            if l['type'] == 'dense':
+                next_size = l['shape'][-1]
+                print(f'Adding Dense layer with size [{prev_size}, {next_size}]')
 
-            if n < n_layers - 1:
-                self.layers.append(tf.nn.tanh)
+                self.layers.append(DenseLayer(batch_size, prev_size, next_size))
+                self.layers[-1].set_weights(l['weights'])
+                prev_size = next_size
+
+                if l['activation'] == 'relu':
+                    print('Adding ReLU activation layer')
+                    self.layers.append(tf.nn.relu)
+                elif l['activation'] == 'tanh':
+                    print('Adding tanh activation layer')
+                    self.layers.append(tf.nn.tanh)
 
     def incident(self, x):
         self.a = x[:, :, 0]
         self.model_in = x
+        # print(self.model_in)
 
     def reflected(self):
-        x = self.layers[0](self.model_in)
-        for l in self.layers[1:]:
+        x = self.model_in
+        for l in self.layers:
             x = l(x)
 
-        self.b = x - self.a
+        self.b = -1 * x # - self.a
+        # print(self.b)
         return self.b
 
 # %%
 class ClipperModel(tf.Module):
-    def __init__(self):
+    def __init__(self, json):
         super(ClipperModel, self).__init__()
-        self.Vs = wdf.ResistiveVoltageSource()
-        self.R = wdf.Resistor(10.0e3)
-        self.P1 = wdf.Parallel(self.Vs, self.R)
+        self.Vs = wdf.ResistiveVoltageSource(4700.0)
+        self.C = wdf.Capacitor(47.0e-9, FS)
+        self.P1 = wdf.Parallel(self.Vs, self.C)
 
-        self.model = RootModel(2, 8, 8, batch_size=n_batches)
+        self.model = RootModel(json, batch_size=n_batches)
 
     def forward(self, input):
         sequence_length = input.shape[1]
@@ -113,7 +136,7 @@ class ClipperModel(tf.Module):
             self.Vs.set_resistance(input[:, i, 1:2])
             self.P1.calc_impedance()
 
-            model_in = tf.concat((self.P1.reflected(), self.P1.R / 1000), axis=1)
+            model_in = tf.concat((self.P1.reflected(), tf.math.log(self.P1.R)), axis=1)
             self.model.incident(tf.transpose(model_in, perm=[0, 2, 1]))
             self.P1.incident(self.model.reflected())
 
@@ -124,7 +147,13 @@ class ClipperModel(tf.Module):
         return output_sequence
 
 # %%
-model = ClipperModel()
+with open("diodeR_test_model.json", "r") as read_file:
+    model_json = json.load(read_file)
+
+model = ClipperModel(model_json)
+
+# %%
+# model = ClipperModel()
 
 def pre_emphasis_filter(x, coeff=0.85):
   return tf.concat([x[0:1], x[1:] - coeff*x[:-1]], axis=0)
@@ -154,14 +183,18 @@ def bounds_loss(target_y, pred_y):
     return tf.math.abs(target_min - pred_min) + tf.math.abs(target_max - pred_max)
 
 mse_loss = tf.keras.losses.MeanSquaredError()
-# loss_func = mse_loss
-loss_func = lambda target, pred: esr_loss(target, pred) \
-    + 5 * avg_loss(target, pred) \
-    + 5 * bounds_loss(target, pred)
-optimizer = tf.keras.optimizers.Nadam(learning_rate=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-9)
+# loss_func = lambda target, pred: esr_loss(target, pred) \
+#     + 5 * avg_loss(target, pred) \
+#     + 5 * bounds_loss(target, pred)
+# loss_func = lambda target, pred: tf.sqrt(esr_loss(target, pred)) + mse_loss(target, pred)
+loss_func = mse_loss
+
+# optimizer = tf.keras.optimizers.Nadam(learning_rate=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-9)
+optimizer = tf.keras.optimizers.Adam(learning_rate=1.0e-5)
 
 # %%
-for epoch in tqdm(range(200)):
+# for epoch in tqdm(range(200)):
+for epoch in tqdm(range(101)):
     with tf.GradientTape() as tape:
         outs = model.forward(data_in_batched)[...,0]
         loss = loss_func(outs, data_target)
@@ -182,68 +215,71 @@ print(f'    Loss: {loss}')
 
 # %%
 outs = model.forward(data_in_batched)[...,0].numpy().flatten()
+
+# %%
 print(outs.shape)
 plt.plot(data_target[:,0])
 plt.plot(outs, '--')
+# plt.ylim(-0.2, 0.2)
+# plt.xlim(1150, 1250)
 
 # %%
-def diode_pair_func(x):
-    a = x[0, 0]
-    nextR = x[0, 1]
-    Vt = 25.85e-3
-    R_Is = nextR * 1.0e-9
-    R_Is_overVt = R_Is / Vt
-    logR_Is_overVt = np.log(R_Is_overVt)
-    lamb = np.sign(a)
-    b = a + 2 * lamb * (R_Is - Vt * wrightomega(logR_Is_overVt + lamb * a / Vt + R_Is_overVt))
-    return np.float32(b)
+# def diode_pair_func(x):
+#     a = x[0, 0]
+#     nextR = x[0, 1]
+#     Vt = 25.85e-3
+#     R_Is = nextR * 1.0e-9
+#     R_Is_overVt = R_Is / Vt
+#     logR_Is_overVt = np.log(R_Is_overVt)
+#     lamb = np.sign(a)
+#     b = a + 2 * lamb * (R_Is - Vt * wrightomega(logR_Is_overVt + lamb * a / Vt + R_Is_overVt))
+#     return np.float32(b)
 
-N = 5000
-test_x = np.linspace(-10, 10, N)
-test_x = np.stack([test_x, np.ones(N) * 1.0e-9]).transpose()
+# N = 5000
+# test_x = np.linspace(-10, 10, N)
+# test_x = np.stack([test_x, np.ones(N) * 1.0e-9]).transpose()
 
-ideal_y = np.zeros(N)
-for n in range(N):
-    ideal_y[n] = diode_pair_func(np.array([test_x[n,:]]))
+# ideal_y = np.zeros(N)
+# for n in range(N):
+#     ideal_y[n] = diode_pair_func(np.array([test_x[n,:]]))
 
-x_in_tt = np.array([test_x[:,0].astype(np.float32)]).transpose()
-y_test = model.model.forward(x_in_tt).numpy().flatten()
+# x_in_tt = np.array([test_x[:,0].astype(np.float32)]).transpose()
+# y_test = model.model.forward(x_in_tt).numpy().flatten()
 
-plt.plot(test_x[:,0], ideal_y)
-plt.plot(test_x[:,0], y_test, '--')
+# plt.plot(test_x[:,0], ideal_y)
+# plt.plot(test_x[:,0], y_test, '--')
 
 # %%
+# def save_model_json(model):
+#     def get_weights(layer):
+#         weights = layer.kernel.numpy()
+#         bias = layer.bias.numpy()
+#         return [weights, bias]
 
-def save_model_json(model):
-    def get_weights(layer):
-        weights = layer.kernel.numpy()
-        bias = layer.bias.numpy()
-        return [weights, bias]
-
-    model_dict = {}
-    model_dict["in_shape"] = 1
-    layers = []
-    for layer in model.layers:
-        if layer ==  tf.nn.tanh:
-            layers[-1]["activation"] = 'tanh'
-            continue
+#     model_dict = {}
+#     model_dict["in_shape"] = 2
+#     layers = []
+#     for layer in model.layers:
+#         if layer == tf.nn.tanh:
+#             layers[-1]["activation"] = 'tanh'
+#             continue
         
-        if isinstance(layer, DenseLayer):
-            layer_dict = {
-                "type": 'dense',
-                "shape": layer.bias.shape[-1],
-                "weights": get_weights(layer)
-            }
-        layers.append(layer_dict)
+#         if isinstance(layer, DenseLayer):
+#             layer_dict = {
+#                 "type": 'dense',
+#                 "shape": layer.bias.shape[-1],
+#                 "weights": get_weights(layer)
+#             }
+#         layers.append(layer_dict)
 
-    model_dict["layers"] = layers
-    return model_dict
+#     model_dict["layers"] = layers
+#     return model_dict
 
-def save_model(model, filename):
-    model_dict = save_model_json(model)
-    with open(filename, 'w') as outfile:
-        json.dump(model_dict, outfile, cls=NumpyArrayEncoder, indent=4)
+# def save_model(model, filename):
+#     model_dict = save_model_json(model)
+#     with open(filename, 'w') as outfile:
+#         json.dump(model_dict, outfile, cls=NumpyArrayEncoder, indent=4)
 
-save_model(model.model, 'clipper.json')
+# save_model(model.model, 'clipper_pot.json')
 
 # %%
--- wdf_py/diodeR_test.py
+++ wdf_py/diodeR_test.py
@@ -6,10 +6,10 @@ import tensorflow as tf
 from model_utils import save_model
 
 # %%
-def diode_pair_func(x, R = 1.0e-9):
+def diode_pair_func(x, R = 1.0e-9, Is = 1.0e-9):
     a = x
     Vt = 25.85e-3
-    R_Is = 1.0e-9 * R
+    R_Is = Is * R
     R_Is_overVt = R_Is / Vt
     logR_Is_overVt = np.log(R_Is_overVt)
     lamb = np.sign(a)
@@ -17,11 +17,11 @@ def diode_pair_func(x, R = 1.0e-9):
     return np.float32(b)
 
 # %%
-N = 500
+N = 1000
 test_x = None
-for R_order in range(-9, 10):
+for R_order in np.linspace(1, 9, 20):
     R = 10**R_order
-    x_vals = np.linspace(-10, 10, N)
+    x_vals = np.linspace(-2.5, 2.5, N)
     in_vals = np.stack([x_vals, np.ones(N) * R]).transpose()
 
     if test_x is None:
@@ -38,23 +38,24 @@ for n in range(len(ideal_y)):
     # multiply by -1 to make the data line up better
     ideal_y[n] = -1 * diode_pair_func(*test_x[n])
 
+# use log of impedance instead of impedance!
 test_x[:,1] = np.log(test_x[:,1])
 
 # %%
 # plt.plot(test_x[:,0] + 5 * np.log(test_x[:,1]), ideal_y)
 plt.plot(test_x[:,0])
-plt.plot(test_x[:,1] / 2)
+# plt.plot(test_x[:,1])
 plt.plot(ideal_y)
 
 # %%
-n_layers = 3
+n_layers = 4
 layer_size = 8
 layer_xs = []
 inputs = tf.keras.Input(shape=(2,))
 for n in range(n_layers):
     layer_in = inputs if n < 1 else layer_xs[n-1]
     layer_width = layer_size if n < n_layers - 1 else 1
-    layer_act = 'relu' if n < n_layers - 1 else 'linear'
+    layer_act = 'tanh' if n < n_layers - 1 else 'linear'
     layer_xs.append(tf.keras.layers.Dense(layer_width,
                                           activation=layer_act,
                                           kernel_initializer='orthogonal')(layer_in))
@@ -63,21 +64,38 @@ diode_model = tf.keras.Model(inputs=inputs, outputs=layer_xs[-1])
 diode_model.summary()
 
 # %%
-loss_func = tf.keras.losses.MeanSquaredError()
+mse_loss = tf.keras.losses.MeanSquaredError()
 
-pre_loss = loss_func(ideal_y, diode_model(test_x)).numpy()
-print(f'Loss before training: {pre_loss}')
+eps = np.finfo(np.float32).eps
+def esr_loss(target_y, predicted_y, emphasis_func=lambda x : x):
+    target_yp = emphasis_func(target_y)
+    pred_yp = emphasis_func(predicted_y)
+    mse = tf.math.reduce_sum(tf.math.square(target_yp - pred_yp))
+    energy = tf.math.reduce_sum(tf.math.square(target_yp))
+    
+    loss_unnorm = mse / tf.cast(energy + eps, tf.float32)
+    return tf.sqrt(loss_unnorm / N)
+
+pre_loss_mse = mse_loss(ideal_y, diode_model(test_x)).numpy()
+pre_loss_esr = esr_loss(ideal_y, diode_model(test_x)).numpy()
+print(f'Loss before training: {pre_loss_mse}, {pre_loss_esr}')
+
+def my_loss(target_y, predicted_y):
+    return mse_loss(target_y, predicted_y) + esr_loss(target_y, predicted_y)
 
 # %%
-optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
-diode_model.compile(optimizer, loss_func)
-diode_model.fit(test_x, ideal_y, epochs=1000)
+optimizer = tf.keras.optimizers.Adam(learning_rate=2e-5)
+diode_model.compile(optimizer, my_loss)
+diode_model.fit(test_x, ideal_y, epochs=2000)
 
 y_test = diode_model(test_x).numpy().flatten()
 
 # %%
 plt.plot(ideal_y)
 plt.plot(y_test, '--')
+
+# plt.xlim(1400, 1600)
+# plt.ylim(-0.25, 0.25)
 
 # %%
 diode_model.save('diodeR_test_model')
@@ -86,3 +104,6 @@ diode_model.save('diodeR_test_model')
 save_model(diode_model, 'diodeR_test_model.json')
 
 # %%
+# val_x = np.array([[0.45355588, 5.2008276]])
+# print(diode_model(val_x))
+# print(-1 * diode_pair_func(val_x[0,0], np.exp(val_x[0,1])))
