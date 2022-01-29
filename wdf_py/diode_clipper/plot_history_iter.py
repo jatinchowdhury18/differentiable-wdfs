78a5ab816dbd8500a0698c0aa87ac298aac358be
christopherclarkeSUTD <christopher_clarke@mymail.sutd.edu.sg>
2022-01-29 21:26:49 +0800

    iter script updates

--- wdf_py/diode_clipper/plot_history_iter.py
+++ wdf_py/diode_clipper/plot_history_iter.py
@@ -5,33 +5,36 @@ import pickle
 name = "Test History"
 
 colrow = {
-    "col":[0,1,2,0,1,2,0,1,2,0,1,2,0,1,2,0,1,2],
-    "row":[0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5],
+    "col":[0,1,2,0,1,2,0,1,2,0,1,2,0,1,2,0,1,2,0,1,2,0,1,2,0,1,2],
+    "row":[0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8],
     }
 
-# learning_rates_dict = {
-#     "learning_rate":[1e-4,1e-4,1e-4,1e-4,1e-4,1e-4,1e-4,1e-4,1e-4,1e-3,1e-3,1e-3,1e-3,1e-3,1e-3,1e-3,1e-3,1e-3],
-#     "beta1":[0.9,0.9,0.9,0.7,0.7,0.7,0.5,0.5,0.5,0.9,0.9,0.9,0.7,0.7,0.7,0.5,0.5,0.5],
-#     "beta2":[0.999,0.8,0.7,0.999,0.8,0.7,0.999,0.8,0.7,0.999,0.8,0.7,0.999,0.8,0.7,0.999,0.8,0.7]
-    
-# }
-
-network_dict = {
-    "n_layer" : [2,2,2,4,4],
-    "layer_size" : [4,8,16,4,8],
+learning_rates_dict = {
+    "learning_rate":[1e-4,1e-4,1e-4,1e-4,1e-4,1e-4,1e-4,1e-4,1e-4,5e-3,5e-3,5e-3,5e-3,5e-3,5e-3,5e-3,5e-3,5e-3,1e-3,1e-3,1e-3,1e-3,1e-3,1e-3,1e-3,1e-3,1e-3],
+    "beta1":[0.9,0.9,0.9,0.7,0.7,0.7,0.5,0.5,0.5,0.9,0.9,0.9,0.7,0.7,0.7,0.5,0.5,0.5,0.9,0.9,0.9,0.7,0.7,0.7,0.5,0.5,0.5],
+    "beta2":[0.999,0.8,0.7,0.999,0.8,0.7,0.999,0.8,0.7,0.999,0.8,0.7,0.999,0.8,0.7,0.999,0.8,0.7,0.999,0.8,0.7,0.999,0.8,0.7,0.999,0.8,0.7]
 }
 
-fig, axs = plt.subplots(2,3)
+
+# network_dict = {
+#     "n_layer" : [2,2,2,4,4],
+#     "layer_size" : [4,8,16,4,8],
+# }
+
+fig, axs = plt.subplots(9,3)
 # fig.suptitle(f'{name} Training History')
-fig.set_size_inches(15,12)
+fig.set_size_inches(16.5,23.5)
 fig.tight_layout()
 fig.subplots_adjust(wspace=0.4, hspace=0.6)
 
 
-for train_num in range(5):
-    plot_num = train_num + 1
-    n_layers = network_dict["n_layer"][train_num]
-    layer_size = network_dict["layer_size"][train_num]
+for train_num in range(27):
+    learning_rate_n = learning_rates_dict["learning_rate"][train_num]
+    beta1n = learning_rates_dict["beta1"][train_num]
+    beta2n = learning_rates_dict["beta2"][train_num]
+    plot_num = f"{train_num+1}_lr_{learning_rate_n}_beta1_{beta1n}_beta2_{beta2n}"
+    n_layers = 2
+    layer_size = 16
     print(plot_num)
     print([colrow["row"][train_num],colrow["col"][train_num]])
     history_file = f"./histories/1N4148 (1U-1D)_{n_layers}x{layer_size}_training_{plot_num}_history.pkl"
@@ -60,9 +63,9 @@ for train_num in range(5):
     ax2.set_ylabel('ESR')
 
     axs[colrow["row"][train_num],colrow["col"][train_num]].legend(handles=[mse_plot, esr_plot])
-    plot_name = f"Layers: {n_layers}, Layer Size = {layer_size}"
-    axs[colrow["row"][train_num],colrow["col"][train_num]].set_title(plot_name, loc="left")
+    plot_name = f"Network: 2x16, Learning rate: {learning_rate_n} Beta1: {beta1n} Beta2: {beta2n}"
+    axs[colrow["row"][train_num],colrow["col"][train_num]].set_title(plot_name, loc="left", fontsize=10, fontweight='bold')
 
 
-fig.savefig("./plots/learningrateplot.jpg")
+fig.savefig("./plots/learningrateplot.eps", format='eps')
 # %%
